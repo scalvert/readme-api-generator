3d357e250d0160434de1ac3a0441787d3b0ffae0
Steve Calvert <scalvert@linkedin.com>
2021-09-13 09:50:24 -0700

    Initial commit

---
+++ __tests__/index-test.js
@@ -0,0 +1,179 @@
+const path = require('path');
+const fs = require('fs');
+const { Project } = require('fixturify-project');
+const execa = require('execa');
+
+class FakeProject extends Project {
+  write(dirJSON) {
+    Object.assign(this.files, dirJSON);
+    this.writeSync();
+  }
+}
+
+describe('readme-api-generator', () => {
+  let project;
+
+  beforeEach(() => {
+    project = new FakeProject();
+
+    project.writeSync();
+  });
+
+  afterEach(() => {
+    project.dispose();
+  });
+
+  it('returns error if no README is present', async () => {
+    let result = await run();
+
+    expect(result.stderr).toMatchInlineSnapshot(
+      `"No README found. Please run readme-api-generator from a directory that contains a README.md file."`
+    );
+    expect(result.exitCode).toEqual(1);
+  });
+
+  it('returns error if README does not contain start or end placeholders', async () => {
+    project.write({
+      'README.md': '',
+    });
+
+    let result = await run();
+
+    expect(result.stderr).toMatchInlineSnapshot(
+      `"The README does not contain a valid placeholder (<!--DOCS_START--> followed by a <!--DOCS_END--> HTML comment)"`
+    );
+    expect(result.exitCode).toEqual(1);
+  });
+
+  it('returns error if README does not contain start placeholder', async () => {
+    project.write({
+      'README.md': '<!--DOCS_END-->',
+    });
+
+    let result = await run();
+
+    expect(result.stderr).toMatchInlineSnapshot(
+      `"The README does not contain a valid placeholder (<!--DOCS_START--> followed by a <!--DOCS_END--> HTML comment)"`
+    );
+    expect(result.exitCode).toEqual(1);
+  });
+
+  it('returns error if README does not contain end placeholder', async () => {
+    project.write({
+      'README.md': '<!--DOCS_START-->',
+    });
+
+    let result = await run();
+
+    expect(result.stderr).toMatchInlineSnapshot(
+      `"The README does not contain a valid placeholder (<!--DOCS_START--> followed by a <!--DOCS_END--> HTML comment)"`
+    );
+    expect(result.exitCode).toEqual(1);
+  });
+
+  it('writes content for single file', async () => {
+    project.write({
+      'README.md': `Fake readme
+<!--DOCS_START--><!--DOCS_END-->`,
+      'protection.js': `/**
+ * A quite wonderful function.
+ * @param {object} - Privacy gown
+ * @param {object} - Security
+ * @returns {survival}
+ */
+function protection (cloak, dagger) {}
+`,
+    });
+
+    let result = await run(['protection.js']);
+
+    expect(result.stdout).toMatchInlineSnapshot(`"README content updated"`);
+    expect(
+      fs.readFileSync(path.join(project.baseDir, 'README.md'), {
+        encoding: 'utf-8',
+      })
+    ).toMatchSnapshot();
+    expect(result.exitCode).toEqual(0);
+  });
+
+  it('writes content for multiple files', async () => {
+    project.write({
+      'README.md': `Fake readme
+<!--DOCS_START--><!--DOCS_END-->`,
+      'protection.js': `/**
+ * A quite wonderful function.
+ * @param {object} - Privacy gown
+ * @param {object} - Security
+ * @returns {survival}
+ */
+function protection (cloak, dagger) {}
+`,
+      'defense.js': `/**
+ * Another wonderful function.
+ * @param {object} - Privacy gown
+ * @param {object} - Security
+ * @returns {alive}
+ */
+function defense (cloak2, dagger2) {}
+`,
+    });
+
+    let result = await run(['protection.js', 'defense.js']);
+
+    expect(result.stdout).toMatchInlineSnapshot(`"README content updated"`);
+    expect(
+      fs.readFileSync(path.join(project.baseDir, 'README.md'), {
+        encoding: 'utf-8',
+      })
+    ).toMatchSnapshot();
+    expect(result.exitCode).toEqual(0);
+  });
+
+  it('writes content for directories', async () => {
+    project.write({
+      'README.md': `Fake readme
+<!--DOCS_START--><!--DOCS_END-->`,
+      lib: {
+        'protection.js': `/**
+   * A quite wonderful function.
+   * @param {object} - Privacy gown
+   * @param {object} - Security
+   * @returns {survival}
+   */
+  function protection (cloak, dagger) {}
+  `,
+        'defense.js': `/**
+   * Another wonderful function.
+   * @param {object} - Privacy gown
+   * @param {object} - Security
+   * @returns {alive}
+   */
+  function defense (cloak2, dagger2) {}
+  `,
+      },
+    });
+
+    let result = await run(['lib']);
+
+    expect(result.stdout).toMatchInlineSnapshot(`"README content updated"`);
+    expect(
+      fs.readFileSync(path.join(project.baseDir, 'README.md'), {
+        encoding: 'utf-8',
+      })
+    ).toMatchSnapshot();
+    expect(result.exitCode).toEqual(0);
+  });
+
+  function run(args = [], options = {}) {
+    let defaults = {
+      reject: false,
+      cwd: project.baseDir,
+    };
+
+    return execa(
+      process.execPath,
+      [require.resolve('../index.js'), ...args],
+      Object.assign({}, defaults, options)
+    );
+  }
+});
---
+++ index.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const fs = require('fs');
+const meow = require('meow');
+const jsdoc2md = require('jsdoc-to-markdown');
+
+const DOCS_PLACEHOLDER = /<!--DOCS_START-->[\S\s]*<!--DOCS_END-->/;
+
+const cli = meow(
+  `
+	Usage
+	  $ readme-api-generator <path to js file(s) or directory>
+
+	Examples
+	  $ readme-api-generator lib/foo.js lib/bar.js
+`
+);
+
+function getFiles(input) {
+  if (
+    input.length === 1 &&
+    path.extname(input[0]) !== '.js' &&
+    fs.existsSync(input[0])
+  ) {
+    return fs
+      .readdirSync(input[0])
+      .filter((file) => path.extname(file) === '.js');
+  } else {
+    return input;
+  }
+}
+
+function getReadme(workingDir) {
+  const readmePath = path.resolve(workingDir, 'README.md');
+
+  try {
+    const readmeContent = fs.readFileSync(readmePath, 'utf8');
+
+    if (!readmeContent.match(DOCS_PLACEHOLDER)) {
+      throw new Error(
+        'The README does not contain a valid placeholder (<!--DOCS_START--> followed by a <!--DOCS_END--> HTML comment)'
+      );
+    }
+
+    return [readmePath, readmeContent];
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error(
+        'No README found. Please run readme-api-generator from a directory that contains a README.md file.'
+      );
+    }
+
+    throw error;
+  }
+}
+
+function writeDocs(readmePath, readmeContent, docsContent) {
+  fs.writeFileSync(
+    readmePath,
+    readmeContent.replace(
+      DOCS_PLACEHOLDER,
+      `<!--DOCS_START-->\n\n${docsContent}\n<!--DOCS_END-->`
+    )
+  );
+}
+
+(async function () {
+  const workingDir = process.cwd();
+
+  try {
+    const [readmePath, readmeContent] = getReadme(workingDir);
+
+    let docsContent = await jsdoc2md.render({
+      files: getFiles(cli.input),
+    });
+
+    writeDocs(readmePath, readmeContent, docsContent);
+
+    console.log('README content updated');
+  } catch (error) {
+    console.error(error.message);
+    process.exitCode = 1;
+  }
+})();
